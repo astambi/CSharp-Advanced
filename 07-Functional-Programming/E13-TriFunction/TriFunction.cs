b98d0883716f3b647ead874d8716824e583cfcf1
Anna <annsta@gmail.com>
2017-06-20 22:28:08 +0300

    Functional Programming

---
+++ 07-Functional-Programming/E13-TriFunction/TriFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace E13_TriFunction
+{
+    public class TriFunction
+    {
+        public static void Main()
+        {
+            var sum = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                        .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+            Func<string, int> sumChars = str => str.ToCharArray().Sum(ch => ch);
+            Func<string, int, bool> isEqualOrLargerSum = (str, n) => sumChars(str) >= n;
+
+            var name = names.FirstOrDefault(n => isEqualOrLargerSum(n, sum));
+            Console.WriteLine(name);
+        }
+    }
+}
